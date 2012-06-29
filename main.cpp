ae1e15585c7bf0b2cee0afe396693ddcbb1d2e2a
mikespikeita@gmail.com <mikespikeita@gmail.com@919106bd-b25a-2ed2-aa45-acb3e9c1ba24>
2012-06-29 16:14:28 +0000

    main.cpp: aggiunta funzione di callback per i punti che printa indice e coordinate del punto selezionado con shift+click

--- main.cpp
+++ main.cpp
@@ -22,33 +22,34 @@ void pointPickCallback(const pcl::visualization::PointPickingEvent& event, void*
 int main(int argc, char *argv[])
 {
 //    WINDOW TEST
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
 
 //    REGISTRATION TEST
-//    pcl::PointCloud<pcl::PointXYZRGB>::Ptr source (new pcl::PointCloud<pcl::PointXYZRGB>);
-//    pcl::PointCloud<pcl::PointXYZRGB>::Ptr target (new pcl::PointCloud<pcl::PointXYZRGB>);
-//    pcl::PointCloud<pcl::PointXYZRGB>::Ptr registered (new pcl::PointCloud<pcl::PointXYZRGB>);
-//    pcl::io::loadPCDFile ("source.pcd", *source);
-//    pcl::io::loadPCDFile ("target.pcd", *target);
-//    int verbosity=1,compute=1;
-//    cout << "Be verbose? "<<flush;
-//    scanf ("%d",&verbosity);
-//    cout << "Compute target? "<<flush;
-//    scanf ("%d",&compute);
-//    registerSourceToTarget(source, target, registered, verbosity, compute);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr source (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr target (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr registered (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile ("source.pcd", *source);
+    pcl::io::loadPCDFile ("target.pcd", *target);
+    int verbosity=1,compute=1;
+    cout << "Be verbose? "<<flush;
+    scanf ("%d",&verbosity);
+    cout << "Compute target? "<<flush;
+    scanf ("%d",&compute);
+    registerSourceToTarget(source, target, registered, verbosity, compute);
 
 //    VisualizationXYZRGB
-//    pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-//    viewer.setBackgroundColor (0, 0, 0);
-//    viewer.initCameraParameters ();
-//    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(registered);
-//    viewer.addPointCloud<pcl::PointXYZRGB> (registered, rgb, "source_registered");
-//    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb2(target);
-//    viewer.addPointCloud<pcl::PointXYZRGB> (target, rgb2, "target_reference");
-//    while (!viewer.wasStopped ())	{viewer.spin ();}
+    pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+    viewer.setBackgroundColor (0, 0, 0);
+    viewer.initCameraParameters ();
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(registered);
+    viewer.addPointCloud<pcl::PointXYZRGB> (registered, rgb, "source_registered");
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb2(target);
+    viewer.addPointCloud<pcl::PointXYZRGB> (target, rgb2, "target_reference");
+    viewer.registerPointPickingCallback (&pointPickCallback, 0);
+    while (!viewer.wasStopped ())	{viewer.spin ();}
 
 
 }
