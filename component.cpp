d65e29dce735776ff8b80677404bb6e1c9ba515e
mikespikeita@gmail.com <mikespikeita@gmail.com@919106bd-b25a-2ed2-aa45-acb3e9c1ba24>
2012-07-17 14:33:44 +0000

    formattazione

--- component.cpp
+++ component.cpp
@@ -24,52 +24,64 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr Component::getCloud()
 {
     return cloud;
 }
+
 pcl::PointIndices::Ptr Component::getIndices()
 {
     return componentIndices;
 }
+
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr Component::getComponentCloud()
 {
    pcl::PointCloud<pcl::PointXYZRGB>::Ptr componentCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
    pcl::copyPointCloud(*cloud,*componentIndices,*componentCloud);
    return componentCloud;
 }
+
 int Component::getGeneratingIndex()
 {
     return generatingIndex;
 }
+
 int Component::getColorThreshold()
 {
     return colorThreshold;
 }
+
 double Component::getClusterThreshold()
 {
     return clusterThreshold;
 }
+
 int Component::getColR()
 {
     return cloud->at(generatingIndex).r;
 }
+
 int Component::getColG()
 {
     return cloud->at(generatingIndex).g;
 }
+
 int Component::getColB()
 {
     return cloud->at(generatingIndex).b;
 }
+
 float Component::getPosX()
 {
     return cloud->at(generatingIndex).x;
 }
+
 float Component::getPosY()
 {
     return cloud->at(generatingIndex).y;
 }
+
 float Component::getPosZ()
 {
     return cloud->at(generatingIndex).z;
 }
+
 int Component::getSize()
 {
     return componentIndices->indices.size();
